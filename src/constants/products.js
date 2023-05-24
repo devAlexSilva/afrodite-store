836791c7011472f11e4ddc6ddec26f860078e053
Alex Kitsu <9apsilva@gmail.com>
2023-05-24 18:00:03 -0300

    feat: added tailwind styles to home and detail product

---
+++ src/constants/products.js
@@ -0,0 +1,48 @@
+import img1 from '/public/calcados-01.jpg'
+export const products = [
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://source.unsplash.com/random/600x400/?sapato',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 2,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://source.unsplash.com/random/600x400/?sapatos',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 3,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://source.unsplash.com/random/600x400/?sandalia',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 4,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://source.unsplash.com/random/600x400/?chinelo',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 5,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://source.unsplash.com/random/600x400/?calcado',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  }
+]

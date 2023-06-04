c9b9675424d24ac617394343c8092801312ff915
Alex Kitsu <9apsilva@gmail.com>
2023-06-04 15:39:54 -0300

    refactor: removed schemas unecessary

--- sanity/schema.js
+++ sanity/schema.js
@@ -1,9 +1,7 @@
 import blockContent from './schemas/blockContent'
 import category from './schemas/category'
-import post from './schemas/post'
-import author from './schemas/author'
 import product from './schemas/product'
 
 export const schema = {
-  types: [post, author, category, blockContent, product],
+  types: [category, blockContent, product],
 }
